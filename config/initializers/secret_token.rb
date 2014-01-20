c7f44738ce455feab42ea1fe73ce0aabbaa95c3d
kevlarmusik <kevlar.eq@gmail.com>
2014-01-20 15:52:12 -0500

    Set up a new rails project called unicorn_factory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnicornFactory::Application.config.secret_key_base = '1c2ad8af619bff7451238aa7edece63fce9917d8f4b5ccd171ba6759abe91f318335035aaa8de8aaab965926b7cda1b8db28d6882971e197647bd28d6a22383d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UnicornFactory::Application.config.session_store :cookie_store, key: '_unicorn_factory_session'
